4a1a0808e5b7d04350b622eda4a1769b1c86066f
Dema <liridon.dema@adesso.de>
2024-05-19 22:20:35 +0200

    Initial commit

---
+++ client/src/app/pages/(home).page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'client-home',
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: `
+     <client-analog-welcome/>
+  `,
+})
+export default class HomeComponent {
+}
