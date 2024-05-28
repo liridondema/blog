0431800d15a6db64615b42cd751fb26b8eb736cf
Dema <liridon.dema@adesso.de>
2024-05-28 13:58:07 +0200

    nestjs module refactored

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -4,23 +4,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Post } from './posts/entities/post.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     PostsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'adminLiridon.1',
-      database: 'blog',
-      entities: [Post],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Post],
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
